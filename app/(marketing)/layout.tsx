a9b39f5ad60540e02a066796b8110480e007801d
Manan Khurana <khuranamanan12@gmail.com>
2024-03-16 17:42:47 +0530

    testing colors

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,13 +1,13 @@
-import Link from "next/link"
+import Link from "next/link";
 
-import { marketingConfig } from "@/config/marketing"
-import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button"
-import { MainNav } from "@/components/main-nav"
-import { SiteFooter } from "@/components/site-footer"
+import { marketingConfig } from "@/config/marketing";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+import { MainNav } from "@/components/main-nav";
+import { SiteFooter } from "@/components/site-footer";
 
 interface MarketingLayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default async function MarketingLayout({
@@ -16,6 +16,7 @@ export default async function MarketingLayout({
   return (
     <div className="flex min-h-screen flex-col">
       <header className="container z-40 bg-background">
+        {/* <header className="container z-40 bg-background/10 backdrop-blur-xl"> */}
         <div className="flex h-20 items-center justify-between py-6">
           <MainNav items={marketingConfig.mainNav} />
           <nav>
@@ -34,5 +35,5 @@ export default async function MarketingLayout({
       <main className="flex-1">{children}</main>
       <SiteFooter />
     </div>
-  )
+  );
 }
